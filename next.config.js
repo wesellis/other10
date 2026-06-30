d8a415855e5105c4b84ba278de5348b00f3c7881
Wesley Ellis <wes@wesellis.com>
2026-06-30 14:29:59 -0400

    chore: add GitHub Pages deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+  basePath: isGitHubPages ? '/other10' : '',
+  assetPrefix: isGitHubPages ? '/other10/' : '',
 };
 
 module.exports = nextConfig;
